27910225129e93faa2a54eb5d9f800e9235295f3
kolaadetola <kolawoleadetola630@gmail.com>
2026-09-10 11:09:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const https = require('https')
+
+const params = JSON.stringify({
+  "email": "kolawoleadetola630@gmail.com",
+  "amount": "500000"
+})
+
+const options = {
+  hostname: 'api.paystack.co',
+  port: 443,
+  path: '/transaction/initialize',
+  method: 'POST',
+  headers: {
+    Authorization: 'Bearer sk_live_115a286c5675a66dc5dc02f207761cb99c1b19f2',
+    'Content-Type': 'application/json'
+  }
+}
+
+const req = https.request(options, res => {
+  let data = ''
+
+  res.on('data', (chunk) => {
+    data += chunk
+  });
+
+  res.on('end', () => {
+    console.log(JSON.parse(data))
+  })
+}).on('error', error => {
+  console.error(error)
+})
+
+req.write(params)
+req.end()
